2a6e77ca81ff1541c37daa4eeec4bb2811c12209
omrialt <96864093+omrialt@users.noreply.github.com>
2022-06-19 16:17:10 +0300

    Improve UI modal

--- src/components/Home/Modal/Modal.js
+++ src/components/Home/Modal/Modal.js
@@ -1,6 +1,7 @@
 import classes from "./Modal.module.css";
 import { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { Card } from "react-bootstrap";
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onClose} />;
@@ -8,9 +9,9 @@ const Backdrop = (props) => {
 
 const ModalOverlay = (props) => {
   return (
-    <div className={classes.modal}>
+    <Card className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
-    </div>
+    </Card>
   );
 };
 
@@ -21,10 +22,13 @@ const Modal = (props) => {
     <Fragment>
       {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalEL)}
       {ReactDOM.createPortal(
-        <ModalOverlay>{props.children}</ModalOverlay>,
+        <div className={classes.centered}>
+          <ModalOverlay>{props.children}</ModalOverlay>
+        </div>,
         portalEL
       )}
     </Fragment>
   );
 };
 export default Modal;
+
